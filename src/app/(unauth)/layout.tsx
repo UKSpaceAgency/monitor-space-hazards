160bd4dd33481d167337de58efe4acec86f671f3
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-11-08 14:06:32 +0100

    fix(make common layout): make common layout

--- src/app/(unauth)/layout.tsx
+++ src/app/(unauth)/layout.tsx
@@ -1,12 +1,7 @@
-import { BaseTemplate } from '@/templates/BaseTemplate';
-
 export default async function UnAuthLayout({
   children,
 }: { children: React.ReactNode }) {
   return (
-    <BaseTemplate>
-      {children}
-    </BaseTemplate>
-
+    <>{children}</>
   );
 }
