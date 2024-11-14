47a69ad0e87ecee702b717637f5db3fe96bb7967
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-11-14 12:23:39 +0100

    feat(ss-2503): protect page

--- src/app/(auth)/account/alert-settings/page.tsx
+++ src/app/(auth)/account/alert-settings/page.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
 import { getUsersMe } from '@/actions/getUsersMe';
 import { getUsersMeAlertSettings } from '@/actions/getUsersMeAlertSettings';
 import { AlertSettingsForm } from '@/components/account/alert-settings/AlertSettingsForm';
+import { isSatteliteUser } from '@/utils/Roles';
 
 export const metadata: Metadata = {
   title: 'Edit your alert settings',
@@ -12,9 +14,9 @@ export default async function AlertSettingsPage() {
   const user = await getUsersMe();
   const alertSettings = await getUsersMeAlertSettings();
 
-  // if (!isSatteliteUser(user.role) || !user.id) {
-  //   return notFound();
-  // }
+  if (isSatteliteUser(user.role) || !user.id) {
+    return notFound();
+  }
 
   const defaultValues = {
     conjunctionAlerts: alertSettings.conjunction_alert_settings?.chosen_option || 'none',
@@ -26,7 +28,7 @@ export default async function AlertSettingsPage() {
 
   return (
     <div>
-      <AlertSettingsForm userId={user.id!} defaultValues={defaultValues} />
+      <AlertSettingsForm userId={user.id} defaultValues={defaultValues} />
     </div>
   );
 }
