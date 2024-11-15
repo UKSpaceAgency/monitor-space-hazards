4f87b5420fe662a2a37f945d3dc76db8b7136554
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-11-15 11:41:50 +0100

    fix(ss-2503): zod fix

--- src/validations/alertSettingsSchema.ts
+++ src/validations/alertSettingsSchema.ts
@@ -1,28 +1,33 @@
 import { z } from 'zod';
 
+const receiveDefault = z.array(z.enum([
+  'SMS',
+  'EMAIL',
+]));
+
 export const alertSettingsSchema = z.object({
   conjunctionAlerts: z.union([
     z.literal('all'),
     z.literal('none'),
     z.literal('priority'),
   ]),
-  receiveConjunction: z.array(z.union([z.literal('EMAIL'), z.literal('SMS')])),
+  receiveConjunction: receiveDefault,
   reEntryAlerts: z.union([
     z.literal('all'),
     z.literal('none'),
     z.literal('priority'),
     z.literal('uk_satellites_only'),
   ]),
-  receiveReEntry: z.array(z.union([z.literal('EMAIL'), z.literal('SMS')])),
-  areasOfInterest: z.array(z.union([
-    z.literal('ENGLAND'),
-    z.literal('NORTHERN_IRELAND'),
-    z.literal('SCOTLAND'),
-    z.literal('WALES'),
-    z.literal('BRITISH_OVERSEAS_TERRITORIES'),
-    z.literal('SHANWICK'),
-    z.literal('NAVAREA'),
-    z.literal('REST_OF_THE_WORLD'),
+  receiveReEntry: receiveDefault,
+  areasOfInterest: z.array(z.enum([
+    'ENGLAND',
+    'NORTHERN_IRELAND',
+    'SCOTLAND',
+    'WALES',
+    'BRITISH_OVERSEAS_TERRITORIES',
+    'SHANWICK',
+    'NAVAREA',
+    'REST_OF_THE_WORLD',
   ])),
 });
 
