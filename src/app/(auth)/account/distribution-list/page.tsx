9285641409c5eb62df319eb791636e3417c5b4a8
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-11-15 10:08:44 +0100

    feat(ss-2504): accordion usage

--- src/app/(auth)/account/distribution-list/page.tsx
+++ src/app/(auth)/account/distribution-list/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 
+import { DistributionListAccordion } from '@/components/account/distribution-list/DistributionListAccordion';
+
 export const metadata: Metadata = {
   title: 'View distribution lists',
 };
@@ -11,6 +13,17 @@ export default async function DistributionListsPage() {
   return (
     <div>
       <h1 className="govuk-heading-xl">{t('title')}</h1>
+      <p className="govuk-body">{t('description')}</p>
+
+      <h2 className="govuk-heading-l">
+        {t('distribution_list', { type: 'conjunction' })}
+      </h2>
+
+      <DistributionListAccordion />
+
+      <h2 className="govuk-heading-l">
+        {t('distribution_list', { type: 're-entry' })}
+      </h2>
     </div>
   );
 }
---
+++ src/components/account/distribution-list/DistributionListAccordion.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+
+import type { TypeAlertSettingsDistributionList } from '@/__generated__/data-contracts';
+import { DataTable } from '@/components/DataTable';
+import type { TranslatedColumnDef } from '@/types';
+import Accordion from '@/ui/accordion/accordion';
+
+const DistributionListAccordion = () => {
+  const t = useTranslations('Accordions.DistributionLists');
+
+  const columns: TranslatedColumnDef<TypeAlertSettingsDistributionList>[] = [
+    {
+      accessorKey: 'name',
+      id: 'name',
+      header: 'Name',
+      enableSorting: false,
+      cell: ({ row }) =>
+        `${row.original.first_name} ${row.original.last_name}`,
+    },
+    {
+      accessorKey: 'email',
+      id: 'email',
+      header: 'Email',
+      enableSorting: false,
+    },
+    {
+      accessorKey: 'organizationName',
+      id: 'organizationName',
+      header: 'Organisation/department',
+      enableSorting: false,
+    },
+    {
+      accessorKey: 'phoneNumber:',
+      id: 'phoneNumber:',
+      enableSorting: false,
+      header: 'Phone number',
+    },
+    {
+      id: 'userId',
+      accessorKey: `userId`,
+      enableSorting: false,
+      header: () => <span style={{ visibility: 'hidden' }}>Action</span>,
+      cell: ({ getValue }: any) => (
+        <Link
+          href={`/account/alert-settings/${getValue() as string}`}
+          className="govuk-link"
+        >
+          Edit
+        </Link>
+      ),
+    },
+  ];
+
+  return (
+    <Accordion
+      id="conjunction-list"
+      items={[
+        {
+          heading: t('receive_alerts', { item: 'all conjunction' }),
+          content: (
+            <DataTable data={[]} columns={columns} />
+          ),
+        },
+        {
+          heading: t('receive_alerts', { item: 'only priority conjunction' }),
+          content: (
+            <DataTable data={[]} columns={columns} />
+          ),
+        },
+      ]}
+    />
+  );
+};
+
+export { DistributionListAccordion };
