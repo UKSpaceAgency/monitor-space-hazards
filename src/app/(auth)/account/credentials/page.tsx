ebfe9e7fedc11aacfa645616ffcc6ecbbfbda792
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-11-27 14:56:53 +0100

    fix(ss-2539-fix): copy fixes

--- src/app/(auth)/account/credentials/page.tsx
+++ src/app/(auth)/account/credentials/page.tsx
@@ -4,6 +4,7 @@ import { getTranslations } from 'next-intl/server';
 import { postUsersMeClientCredentials } from '@/actions/postUsersMeClientCredentials';
 import Button from '@/ui/button/button';
 import Panel from '@/ui/panel/panel';
+import WarningText from '@/ui/warning-text/warning-text';
 
 export const metadata: Metadata = {
   title: 'Credentials for API authentication',
@@ -14,33 +15,38 @@ export default async function Credentials() {
   const credentials = await postUsersMeClientCredentials();
 
   return (
-    <Panel heading={t('title')}>
-      <div className="m-9">
-        <p>
-          {t('client_id')}
-          <br />
-          <strong>{credentials.client_id}</strong>
-        </p>
-      </div>
-      <Button
-        className="govuk-button--secondary"
-      >
-        {t('copy_client_id_button')}
-      </Button>
-      <div className="m-9">
-        <p>
-          {t('client_secret')}
-          <br />
-          <strong style={{ lineBreak: 'anywhere' }}>
-            {credentials.client_secret}
-          </strong>
-        </p>
-      </div>
-      <Button
-        className="govuk-button--secondary"
-      >
-        {t('copy_client_secret_button')}
-      </Button>
-    </Panel>
+    <>
+      <Panel heading={t('title')}>
+        <div className="m-9">
+          <p>
+            {t('client_id')}
+            <br />
+            <strong>{credentials.client_id}</strong>
+          </p>
+        </div>
+        <Button
+          className="govuk-button--secondary"
+        >
+          {t('copy_client_id_button')}
+        </Button>
+        <div className="m-9">
+          <p>
+            {t('client_secret')}
+            <br />
+            <strong style={{ lineBreak: 'anywhere' }}>
+              {credentials.client_secret}
+            </strong>
+          </p>
+        </div>
+        <Button
+          className="govuk-button--secondary"
+        >
+          {t('copy_client_secret_button')}
+        </Button>
+      </Panel>
+      <WarningText>
+        {t('warning')}
+      </WarningText>
+    </>
   );
 }
