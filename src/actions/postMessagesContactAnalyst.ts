659e3392f141f46998ee2c286a2c93f363fa485c
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-11-29 16:10:32 +0100

    feat(ss-2565): fix json problem

--- src/actions/postMessagesContactAnalyst.ts
+++ src/actions/postMessagesContactAnalyst.ts
@@ -4,6 +4,5 @@ import type { TypeContactAnalystIn } from '@/__generated__/data-contracts';
 import Api from '@/libs/Api';
 
 export async function postMessagesContactAnalyst(payload: TypeContactAnalystIn) {
-  const data = await Api.postMessagesContactAnalyst(payload);
-  return data;
+  await Api.postMessagesContactAnalyst(payload);
 };
---
+++ src/app/(unauth)/contact-analyst/send/page.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import { getTranslations } from 'next-intl/server';
+
+import Button from '@/ui/button/button';
+import Panel from '@/ui/panel/panel';
+
+export const metadata: Metadata = {
+  title: 'Contact a UKSA orbital analyst',
+};
+
+export default async function ContactAnalystSend({ searchParams }: { searchParams: { callback: string } }) {
+  const t = await getTranslations('ContactAnalystSend');
+  const tCommon = await getTranslations('Common');
+
+  const { callback } = searchParams;
+
+  return (
+    <>
+      <Panel heading={t('title')}>{t('content')}</Panel>
+      <Link href={callback}>
+        <Button>{tCommon('return', { to: 'Event' })}</Button>
+      </Link>
+    </>
+  );
+}
