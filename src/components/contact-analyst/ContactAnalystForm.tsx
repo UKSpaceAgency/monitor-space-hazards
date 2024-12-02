e1615778e5b14d29332dfab44a71a0cce72b8216
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-12-02 11:58:31 +0100

    feat(ss-2565): form fix

--- src/components/contact-analyst/ContactAnalystForm.tsx
+++ src/components/contact-analyst/ContactAnalystForm.tsx
@@ -1,10 +1,9 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/navigation';
 import { useTranslations } from 'next-intl';
 import type { SubmitHandler } from 'react-hook-form';
-import { FormProvider, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 import { postMessagesContactAnalyst } from '@/actions/postMessagesContactAnalyst';
 import Button from '@/ui/button/button';
@@ -23,40 +22,33 @@ type ContactAnalystFormProps = {
 
 const ContactAnalystForm = ({ searchParams }: ContactAnalystFormProps) => {
   const t = useTranslations('Forms.ContactAnalyst');
-  const { push } = useRouter();
 
   const { id, callback } = searchParams;
 
-  const methods = useForm<ContactAnalystSchema>({
+  const { register, handleSubmit, formState: { errors } } = useForm<ContactAnalystSchema>({
     defaultValues: contactAnalystFormDefaultValues,
     resolver: zodResolver(contactAnalyst),
   });
-
-  const { register, handleSubmit, formState: { errors } } = methods;
 
   const onSubmit: SubmitHandler<ContactAnalystSchema> = async ({ messageContent }) => {
     await postMessagesContactAnalyst({
       eventId: id,
       messageContent,
-    });
-
-    push(`/contact-analyst/send?callback=${callback}`);
+    }, callback);
   };
 
   return (
-    <FormProvider {...methods}>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <FormErrorSummary i18path="ContactAnalyst" errors={errors} />
-        <TextArea
-          {...register('messageContent')}
-          id="message"
-          error={errors.messageContent?.message}
-        />
-        <Button type="submit">{t('submit')}</Button>
-      </form>
-    </FormProvider>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <FormErrorSummary i18path="ContactAnalyst" errors={errors} />
+      <TextArea
+        {...register('messageContent')}
+        id="message"
+        error={errors.messageContent?.message}
+      />
+      <Button type="submit">{t('submit')}</Button>
+    </form>
   );
 };
 
