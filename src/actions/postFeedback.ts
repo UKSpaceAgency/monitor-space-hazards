37cd150aa98ca2fa71ef51d6a1d9af99f860f5b5
mkccs <marcin.kucjas@thepsc.co.uk>
2024-12-02 13:06:44 +0100

    fix(ss-2564): client side validation (#44)

--- src/actions/postFeedback.ts
+++ src/actions/postFeedback.ts
@@ -9,45 +9,11 @@ type PostFeedbackPayloadType = {
   details: string;
 };
 
-type FeedbackErrorType = {
-  href: '#details' | '#satisfaction';
-  errorMessage: string;
-};
+export async function postFeedback(data: PostFeedbackPayloadType) {
+  const formData = new FormData();
 
-export async function postFeedback(_prevState: any, formData: FormData) {
-  const data: PostFeedbackPayloadType = {
-    satisfaction: formData.get('satisfaction') as string,
-    details: formData.get('details') as string,
-  };
-
-  const errors: FeedbackErrorType[] = [];
-
-  if (!data.satisfaction) {
-    errors.push({
-      errorMessage: 'You must select one option',
-      href: '#satisfaction',
-    });
-  }
-
-  if (!data.details) {
-    errors.push({
-      errorMessage: 'Field is required',
-      href: '#details',
-    });
-  }
-
-  if (data.details?.length > 2000) {
-    errors.push({
-      errorMessage: 'Your text must be 2000 characters or fewer.',
-      href: '#details',
-    });
-  }
-
-  if (errors.length) {
-    return {
-      errors,
-    };
-  }
+  formData.append('Satisfaction', data.satisfaction);
+  formData.append('Details', data.details);
 
   await fetch(env.FEEDBACK_URL, {
     method: 'POST',
