abeeadbfb01edecb0921e23aece7e85f10d3fb24
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-12-02 10:48:38 +0100

    feat(ss-2564): review fixes

--- src/actions/postFeedback.ts
+++ src/actions/postFeedback.ts
@@ -1,8 +1,58 @@
 'use server';
 
-export async function postFeedback(formData: FormData) {
-  await fetch('https://getform.io/f/8eabf249-d63d1-a035-e1ba5d637cb3', {
+import { redirect } from 'next/navigation';
+
+import { env } from '../../src/libs/Env';
+
+type PostFeedbackPayloadType = {
+  satisfaction: string;
+  details: string;
+};
+
+type FeedbackErrorType = {
+  href: '#details' | '#satisfaction';
+  errorMessage: string;
+};
+
+export async function postFeedback(_prevState: any, formData: FormData) {
+  const data: PostFeedbackPayloadType = {
+    satisfaction: formData.get('satisfaction') as string,
+    details: formData.get('details') as string,
+  };
+
+  const errors: FeedbackErrorType[] = [];
+
+  if (!data.satisfaction) {
+    errors.push({
+      errorMessage: 'You must select one option',
+      href: '#satisfaction',
+    });
+  }
+
+  if (!data.details) {
+    errors.push({
+      errorMessage: 'Field is required',
+      href: '#details',
+    });
+  }
+
+  if (data.details?.length > 2000) {
+    errors.push({
+      errorMessage: 'Your text must be 2000 characters or fewer.',
+      href: '#details',
+    });
+  }
+
+  if (errors.length) {
+    return {
+      errors,
+    };
+  }
+
+  await fetch(env.FEEDBACK_URL, {
     method: 'POST',
     body: formData,
   });
+
+  redirect(`/feedback/success`);
 };
--- src/components/feedback/FeedbackForm.tsx
+++ src/components/feedback/FeedbackForm.tsx
@@ -1,110 +1,83 @@
 'use client';
 
-import { zodResolver } from '@hookform/resolvers/zod';
 import { useTranslations } from 'next-intl';
-import { useState } from 'react';
-import type { SubmitHandler } from 'react-hook-form';
-import { FormProvider, useForm } from 'react-hook-form';
+import { useFormState, useFormStatus } from 'react-dom';
+import { useForm } from 'react-hook-form';
 
 import { postFeedback } from '@/actions/postFeedback';
-import { FormErrorSummary } from '@/components/form/FormErrorSummary';
 import Button from '@/ui/button/button';
+import ErrorSummary from '@/ui/error-summary/error-summary';
 import Fieldset, { } from '@/ui/fieldset/fieldset';
-import Panel from '@/ui/panel/panel';
 import Radios from '@/ui/radios/radios';
 import TextArea from '@/ui/text-area/text-area';
-import { feedBackFormDefaultValues, type FeedbackSchema, feedbackSchema } from '@/validations/feedbackSchema';
+import type { FeedbackSchema } from '@/validations/feedbackSchema';
+import { feedBackFormDefaultValues } from '@/validations/feedbackSchema';
 
 const FeedbackForm = () => {
   const t = useTranslations('Forms.Feedback');
 
-  const [submitted, setSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const methods = useForm<FeedbackSchema>({
+  const { register } = useForm<FeedbackSchema>({
     defaultValues: feedBackFormDefaultValues,
-    resolver: zodResolver(feedbackSchema),
   });
 
-  const { register, handleSubmit, formState: { errors } } = methods;
-
-  const onSubmit: SubmitHandler<FeedbackSchema> = async (data) => {
-    setLoading(true);
-
-    const formData = new FormData();
-    formData.append('Satisfaction', data.satisfaction!);
-    formData.append('Details', data.details);
-
-    try {
-      await postFeedback(formData);
-
-      setSubmitted(true);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      console.error('Unexpected error: ', error);
-    }
-  };
+  const [state, formAction] = useFormState(postFeedback, null);
+  const { pending } = useFormStatus();
 
   return (
-    <>
-      {submitted
-        ? <Panel heading={t('success')} />
-        : (
-            <>
-              <h1 className="govuk-heading-xl">{t('title')}</h1>
-              <FormProvider {...methods}>
-                <form
-                  onSubmit={handleSubmit(onSubmit)}
-                >
-                  <FormErrorSummary i18path="Feedback" errors={errors} />
-                  <Fieldset
-                    legend={{
-                      text: t('survey_label'),
-                    }}
-                  >
-                    <Radios
-                      id="satisfaction"
-                      label={t('radios_label')}
-                      labelClass="govuk-fieldset__legend--m"
-                      error={errors.satisfaction?.message}
-                      items={[{
-                        value: '5',
-                        children: t('very_satisfied'),
-                        ...register('satisfaction'),
-                      }, {
-                        value: '4',
-                        children: t('satisfied'),
-                        ...register('satisfaction'),
-                      }, {
-                        value: '3',
-                        children: t('neither'),
-                        ...register('satisfaction'),
-                      }, {
-                        value: '2',
-                        children: t('dissatisfied'),
-                        ...register('satisfaction'),
-                      }, {
-                        value: '1',
-                        children: t('very_dissatisfied'),
-                        ...register('satisfaction'),
-                      }]}
-                    />
-                  </Fieldset>
-                  <TextArea
-                    {...register('details')}
-                    id="details"
-                    label={t('textarea_label')}
-                    labelClass="govuk-fieldset__legend--m"
-                    hint={t('textarea_hint')}
-                    error={errors.details?.message}
-                  />
-                  <Button type="submit" disabled={loading}>{t('submit')}</Button>
-                </form>
-              </FormProvider>
-            </>
-          )}
-    </>
+    <form
+      action={formAction}
+    >
+      {state?.errors && (
+        <ErrorSummary
+          errorList={state.errors.map(error => ({
+            href: error.href,
+            children: error.errorMessage,
+          }))}
+        />
+      )}
+      <Fieldset
+        legend={{
+          text: t('survey_label'),
+        }}
+      >
+        <Radios
+          id="satisfaction"
+          label={t('radios_label')}
+          labelClass="govuk-fieldset__legend--m"
+          error={state?.errors.find(error => error.href === '#satisfaction')?.errorMessage}
+          items={[{
+            value: '5',
+            children: t('very_satisfied'),
+            ...register('satisfaction'),
+          }, {
+            value: '4',
+            children: t('satisfied'),
+            ...register('satisfaction'),
+          }, {
+            value: '3',
+            children: t('neither'),
+            ...register('satisfaction'),
+          }, {
+            value: '2',
+            children: t('dissatisfied'),
+            ...register('satisfaction'),
+          }, {
+            value: '1',
+            children: t('very_dissatisfied'),
+            ...register('satisfaction'),
+          }]}
+        />
+      </Fieldset>
+      <TextArea
+        {...register('details')}
+        id="details"
+        label={t('textarea_label')}
+        labelClass="govuk-fieldset__legend--m"
+        hint={t('textarea_hint')}
+        error={state?.errors.find(error => error.href === '#details')?.errorMessage}
+      />
+      <Button type="submit" disabled={pending}>{t('submit')}</Button>
+    </form>
   );
 };
 
