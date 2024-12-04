509337dab6bb80098d2338f7b673a8c81a042c7f
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-12-04 14:26:23 +0100

    fix(ss-2540): conjucntion fix

--- src/actions/postAnalyses.ts
+++ src/actions/postAnalyses.ts
@@ -3,33 +3,28 @@
 import { revalidateTag } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-import type { TypeBodyCreateAnalysisV1AnalysesPost, TypeHTTPValidationError, TypeUserIdOut } from '@/__generated__/data-contracts';
-import type { HttpResponse } from '@/__generated__/http-client';
+import type { TypeBodyCreateAnalysisV1AnalysesPost } from '@/__generated__/data-contracts';
 import Api from '@/libs/Api';
 
 import { REVALIDATION_TAGS } from './tags';
 
 export async function postAnalyses(_prevState: any, formData: FormData) {
-  const data: TypeBodyCreateAnalysisV1AnalysesPost = {
+  const payload: TypeBodyCreateAnalysisV1AnalysesPost = {
     event_short_id: formData.get('object_id') as string,
     json_file: formData.get('file') as File,
   };
-  try {
-    await Api.postAnalyses(data);
-  } catch (error) {
-    if (error instanceof Response) {
-      // This is how email validation on backend works
-      const { detail } = (error as HttpResponse<TypeUserIdOut, TypeHTTPValidationError>).error;
-      if (typeof detail === 'string') {
-        return {
-          error: detail,
-        };
-      }
-    }
+
+  const response = await Api.postAnalyses(payload);
+
+  const data = response.data as { message: { status: string; details: string }[] };
+
+  // This is how analyses validation on backend works
+  if (data?.message[0]?.status === 'ERROR') {
     return {
-      error: 'An unexpected error occurred.',
+      error: data.message[0].details,
     };
   }
+
   revalidateTag(REVALIDATION_TAGS.GET_ANALYSES);
-  redirect(`/conjunctions/${data.event_short_id}/analysis-upload/successful`);
+  redirect(`/conjunctions/${payload.event_short_id}/analysis-upload/successful`);
 };
