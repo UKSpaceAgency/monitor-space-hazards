9527047e277a585efb270f00b4d1cb58ab76bf99
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-12-09 15:09:31 +0100

    feat(ss-2582): conjunction refactor

---
+++ src/components/conjunction/ConjunctionMissDistanceChart.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+
+import Details from '@/ui/details/details';
+
+import RichText from '../RichText';
+
+type ConjunctionMissDistanceChartProps = {
+  id: string;
+};
+
+const ConjunctionMissDistanceChart = ({ id }: ConjunctionMissDistanceChartProps) => {
+  const t = useTranslations('Accordions.Conjunction.miss_distance_chart');
+
+  return (
+    <div data-pdf={t('title')}>
+      <Details summary={t('help.title')} data-pdf-ignore>
+        <RichText>
+          {tags => t.rich('help.content', {
+            ...tags,
+            link: chunks => <Link href={`/conjunctions/${id}#eventHistory`} className="govuk-link">{chunks}</Link>,
+          }) }
+        </RichText>
+      </Details>
+    </div>
+  );
+};
+
+export { ConjunctionMissDistanceChart };
