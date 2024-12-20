51cdc4b2e9611df964f2ce5d7a97d1b2065e0bab
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-12-20 13:05:14 +0100

    feat(ss-2584): review fixes

--- src/app/(auth)/account/setup/page.tsx
+++ src/app/(auth)/account/setup/page.tsx
@@ -33,7 +33,7 @@ export default async function SetupPage() {
                 {t('notification.view_home')}
               </Button>
             </Link>
-            {isAdmin(user.role) && (
+            {isAdmin(session.user.role) && (
               <Link
                 href="/account/add-new-user"
               >
