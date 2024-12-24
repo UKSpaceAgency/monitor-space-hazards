792858672bbf70a737439574d5ce4dc487c316dc
Marcin Kucjas <marcin.kucjas@thepsc.co.uk>
2024-12-24 11:40:30 +0100

    feat(ss-2607): objects catalogued

--- src/components/performance-monitoring/data-table/PerformanceMonitoringConjunctionsByEventTypeDataTable.tsx
+++ src/components/performance-monitoring/data-table/PerformanceMonitoringConjunctionsByEventTypeDataTable.tsx
@@ -1,16 +1,24 @@
+import { getTranslations } from 'next-intl/server';
+
 import { getStatsEventsType } from '@/actions/getStatsEventsType';
 import { DataTable } from '@/components/DataTable';
+import { DownloadData } from '@/components/DownloadData';
 
 import { conjunctionsByEventTypeColumns } from './PerformanceMonitoringConjunctionsByEventTypeDataTableColumns';
 
 const PerformanceMonitoringConjunctionsByEventTypeDataTable = async () => {
+  const t = await getTranslations('Tables.Performance_monitoring.conjunction_events_by_type');
+
   const data = await getStatsEventsType();
 
   return (
-    <DataTable
-      columns={conjunctionsByEventTypeColumns}
-      data={data}
-    />
+    <>
+      <DataTable
+        columns={conjunctionsByEventTypeColumns}
+        data={data}
+      />
+      <DownloadData type={t('this_table')} params={{}} downloadAction={getStatsEventsType} />
+    </>
   );
 };
 
