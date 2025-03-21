8088c12a20fc7510d258e66808a5b89f077e5a84
Philip <nowakf@gmail.com>
2025-03-21 09:51:37 +0100

    fix: ingestion time for reentries

--- src/components/re-entries/ReentriesEventsTable.tsx
+++ src/components/re-entries/ReentriesEventsTable.tsx
@@ -1,8 +1,11 @@
+import dayjs from 'dayjs';
 import { getTranslations } from 'next-intl/server';
 
 import type { TypeGetReentryEventsParams } from '@/__generated__/data-contracts';
+import { getCdmsLatest } from '@/actions/getCdmsLatest';
 import { getReentryEvents } from '@/actions/getReentryEvents';
 import { getSession } from '@/actions/getSession';
+import { FORMAT_DATE_TIME } from '@/libs/Dayjs';
 import { isAgencyUser, isGovUser, isSatteliteUser } from '@/utils/Roles';
 
 import { SearchBar } from '../SearchBar';
@@ -17,7 +20,7 @@ const ReentriesEventsTable = async ({ initialParams }: ReentriesEventsTableProps
   const t = await getTranslations('Tables.Reentries');
   const session = await getSession();
   const role = session?.user.role;
-
+  const latestCdms = await getCdmsLatest();
   const params: TypeGetReentryEventsParams = {
     ...initialParams,
     sort_by: 'time_window_start',
@@ -32,6 +35,10 @@ const ReentriesEventsTable = async ({ initialParams }: ReentriesEventsTableProps
       <SearchBar label={t('search_bar.label')} placeholder={t('search_bar.placeholder')} />
       <ReentriesEventsTableFilters showFilterRadios={!isSatteliteUser(session?.user.role)} />
       <ReentriesDataTable params={params} initialData={initialData} haveAccessToAlerts={isAgencyUser(role) || isGovUser(role)} />
+      <div className="govuk-inset-text">
+        {t('reentries_events_as_of')}
+        {dayjs(latestCdms.data.updatedAt).format(FORMAT_DATE_TIME)}
+      </div>
     </div>
   );
 };
