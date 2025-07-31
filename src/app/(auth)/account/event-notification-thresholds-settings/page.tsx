a9d80aa5964860b2444e7a27f71067c7dd0d12e1
Philip <nowakf@gmail.com>
2025-07-31 11:29:50 +0200

    fix: permission fix

--- src/app/(auth)/account/event-notification-thresholds-settings/page.tsx
+++ src/app/(auth)/account/event-notification-thresholds-settings/page.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next';
-import { notFound } from 'next/navigation';
 import { getTranslations } from 'next-intl/server';
 
 import { getUsersMe } from '@/actions/getUsersMe';
 import { ThresholdsSettingsForm } from '@/components/account/thresholds-settings/ThresholdsSettingsForm';
-import { isGovUser } from '@/utils/Roles';
 import type { ThresholdsSettingsFormSchema } from '@/validations/thresholdsSettingsFormSchema';
 
 export const metadata: Metadata = {
@@ -15,10 +13,6 @@ export default async function EventNotificationThresholdsSettingsPage() {
   const t = await getTranslations('Event_notification_thresholds_settings');
 
   const data = await getUsersMe();
-
-  if (isGovUser(data.role)) {
-    return notFound();
-  }
 
   const defaultValues = data.notification_thresholds?.reduce((acc, cur) => {
     if (cur.type === 'TIME_TO_EVENT') {
--- src/app/(auth)/account/notification-settings/page.tsx
+++ src/app/(auth)/account/notification-settings/page.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next';
-import { notFound } from 'next/navigation';
 import { getTranslations } from 'next-intl/server';
 
 import { getUsersMe } from '@/actions/getUsersMe';
 import { NotificationSettingsForm } from '@/components/account/notification-settings/NotificationSettingsForm';
-import { isGovUser } from '@/utils/Roles';
 
 export const metadata: Metadata = {
   title: 'Choose your notification settings',
@@ -13,11 +11,7 @@ export const metadata: Metadata = {
 export default async function NotificationSettingsPage() {
   const t = await getTranslations('Notification_settings');
 
-  const { notification_settings, role } = await getUsersMe();
-
-  if (isGovUser(role)) {
-    return notFound();
-  }
+  const { notification_settings } = await getUsersMe();
 
   const defaultValues = {
     on_event_created: notification_settings?.on_event_created ?? [],
