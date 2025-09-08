493e5876d6381f9e6713f726b4821261e0c256a6
mkccs <marcin.kucjas@thepsc.co.uk>
2025-09-08 14:24:24 +0200

    fix: chart colors fixes (#247)

--- src/components/charts/legend/LegendChart.tsx
+++ src/components/charts/legend/LegendChart.tsx
@@ -37,7 +37,7 @@ export const ChartLegend = ({
 
   return (
     <fieldset className="flex flex-col items-center justify-center gap-2 font-sans text-xs md:mx-2 md:text-base" aria-label={`${ariaLabel} Legend`} data-pdf-ignore>
-      {title && <div><legend className="govuk-fieldset__legend m-0 p-1 font-bold text-nowrap">{title}</legend></div>}
+      {title && <div><legend className="govuk-fieldset__legend text-xs md:text-xl m-0 p-1 font-bold text-nowrap ">{title}</legend></div>}
       <ul className="flex flex-wrap items-center m-0 p-0 gap-x-2 list-none justify-center">
         {items.map(({ label, backgroundColor, borderColor }, index) => (
           // eslint-disable-next-line react/no-array-index-key
