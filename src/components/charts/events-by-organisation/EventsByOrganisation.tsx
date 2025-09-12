5a4fdcf0acb25c3c1cede3dc84e6d22462918ab5
mkccs <marcin.kucjas@thepsc.co.uk>
2025-09-12 08:30:17 +0200

    fix: colors fixes (#255)

--- src/components/charts/events-by-organisation/EventsByOrganisation.tsx
+++ src/components/charts/events-by-organisation/EventsByOrganisation.tsx
@@ -45,14 +45,14 @@ export function EventsByOrganizationChart({
           {
             label: t('<1e-3_and_>1e-5'),
             data: data.map(({ medium }) => medium ?? 0),
-            borderColor: chartPalette.midBlue,
-            backgroundColor: chartPalette.midBlue,
+            borderColor: chartPalette.orange,
+            backgroundColor: chartPalette.orange,
           },
           {
             label: t('>1e-3'),
             data: data.map(({ high }) => high ?? 0),
-            borderColor: chartPalette.brightBlue,
-            backgroundColor: chartPalette.brightBlue,
+            borderColor: chartPalette.darkPink,
+            backgroundColor: chartPalette.darkPink,
           },
         ],
       };
