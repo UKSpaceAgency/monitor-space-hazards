990f7f4454556a4e9eec23ec4306e3125e1d267b
Philip <nowakf@gmail.com>
2025-11-10 17:22:14 +0100

    feat: new chart checkboxes

--- src/components/charts/legend/LegendChart.tsx
+++ src/components/charts/legend/LegendChart.tsx
@@ -39,28 +39,33 @@ export const ChartLegend = ({
     <fieldset className="flex flex-col items-center justify-center gap-2 font-sans text-xs md:mx-2 md:text-base" aria-label={`${ariaLabel} Legend`} data-pdf-ignore>
       {title && <div><legend className="govuk-fieldset__legend text-xs md:text-xl m-0 p-1 font-bold text-nowrap ">{title}</legend></div>}
       <ul className="flex flex-wrap items-center m-0 p-0 gap-x-2 list-none justify-center">
-        {items.map(({ label, backgroundColor, borderColor }, index) => (
+        {items.map(({ label, backgroundColor, borderColor }, index) => {
+          const checked = legendStatusMap[index];
+          return (
           // eslint-disable-next-line react/no-array-index-key
-          <li key={`${index}-${label}`} className="block" aria-label={`Show/hide ${ariaLabel} ${label} data`}>
-            <label className={clsx('flex items-center gap-1 m-0 p-1 pointer-events-none', {
-              'pointer-events-auto cursor-pointer': interactive,
-            })}
-            >
-              <input
-                className="absolute opacity-0 cursor-pointer h-0 w-0 peer/legend"
-                type="checkbox"
-                name={label}
-                value={index}
-                checked={legendStatusMap[index]}
-                onChange={handleLegendClick}
-              />
-              <span className="block flex-auto w-5 h-5 border-[1px] border-solid opacity-50 peer-checked/legend:opacity-100 peer-focus-visible/legend:border-4 peer-focus-visible/legend:shadow-[0_0_0_3px_#fd0]" style={{ backgroundColor, borderColor } as CSSProperties} />
-              <span className="line-through peer-checked/legend:no-underline">
-                {label}
-              </span>
-            </label>
-          </li>
-        ))}
+            <li key={`${index}-${label}`} className="block" aria-label={`Show/hide ${ariaLabel} ${label} data`}>
+              <label className={clsx('flex items-center gap-2 m-0 p-2 pointer-events-none', {
+                'pointer-events-auto cursor-pointer': interactive,
+              })}
+              >
+                <input
+                  className="absolute opacity-0 cursor-pointer h-0 w-0 peer"
+                  type="checkbox"
+                  name={label}
+                  value={index}
+                  checked={checked}
+                  onChange={handleLegendClick}
+                />
+                <span className="block flex-auto w-5 h-5 outline outline-2 outline-black rounded-[1px] border-[4px] peer-focus-visible:ring ring-[#fd0] ring-offset-2" style={{ borderColor } as CSSProperties}>
+                  {checked && <span className="block w-full h-full" style={{ backgroundColor } as CSSProperties} />}
+                </span>
+                <span className="line-through peer-checked:no-underline">
+                  {label}
+                </span>
+              </label>
+            </li>
+          );
+        })}
       </ul>
     </fieldset>
   );
--- src/components/charts/legend/PieLegendChart.tsx
+++ src/components/charts/legend/PieLegendChart.tsx
@@ -44,28 +44,33 @@ export const PieChartLegend = ({
     <fieldset className="font-sans text-xs md:mx-2 md:text-base" aria-label={`${ariaLabel} Legend`} data-pdf-ignore>
       {title && <div><legend className="govuk-fieldset__legend text-xs md:text-xl m-0 p-1 font-bold text-nowrap ">{title}</legend></div>}
       <ul className="list-none">
-        {legendItems.map((label, index) => (
+        {legendItems.map((label, index) => {
+          const checked = legendStatusMap[index];
+          return (
           // eslint-disable-next-line react/no-array-index-key
-          <li key={`${index}-${label}`} className="block" aria-label={`Show/hide ${ariaLabel} ${label} data`}>
-            <label className={clsx('flex items-center gap-1 m-0 p-1 pointer-events-none', {
-              'pointer-events-auto cursor-pointer': interactive,
-            })}
-            >
-              <input
-                className="absolute opacity-0 cursor-pointer h-0 w-0 peer/legend"
-                type="checkbox"
-                name={label}
-                value={index}
-                checked={legendStatusMap[index]}
-                onChange={handleLegendClick}
-              />
-              <span className="block w-5 h-5 border-[1px] border-solid opacity-50 peer-checked/legend:opacity-100 peer-focus-visible/legend:border-4 peer-focus-visible/legend:shadow-[0_0_0_3px_#fd0]" style={{ backgroundColor: legendColors[index], borderColor: legendColors[index] } as CSSProperties} />
-              <span className="line-through peer-checked/legend:no-underline">
-                {label}
-              </span>
-            </label>
-          </li>
-        ))}
+            <li key={`${index}-${label}`} className="block" aria-label={`Show/hide ${ariaLabel} ${label} data`}>
+              <label className={clsx('flex items-center gap-2 m-0 p-2 pointer-events-none', {
+                'pointer-events-auto cursor-pointer': interactive,
+              })}
+              >
+                <input
+                  className="absolute opacity-0 cursor-pointer h-0 w-0 peer"
+                  type="checkbox"
+                  name={label}
+                  value={index}
+                  checked={checked}
+                  onChange={handleLegendClick}
+                />
+                <span className="block w-5 h-5 outline outline-2 outline-black rounded-[1px] border-[4px] peer-focus-visible:ring ring-[#fd0] ring-offset-2" style={{ borderColor: legendColors[index] } as CSSProperties}>
+                  {checked && <span className="block w-full h-full" style={{ backgroundColor: legendColors[index] } as CSSProperties} />}
+                </span>
+                <span className="line-through peer-checked:no-underline">
+                  {label}
+                </span>
+              </label>
+            </li>
+          );
+        })}
       </ul>
     </fieldset>
   );
