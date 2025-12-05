b38c7d43633476896207c279b43ba1983c2946c2
Philip <nowakf@gmail.com>
2025-12-05 11:13:59 +0100

    feat: fragmentations chart

--- src/actions/getStatsMonthlyFragmentationEventsByObjectType.ts
+++ src/actions/getStatsMonthlyFragmentationEventsByObjectType.ts
@@ -8,27 +8,46 @@ import { dayjs } from '@/libs/Dayjs';
 
 export type StatsMonthlyFragmentationEventsByObjectType = {
   month: string;
-  DEBRIS: number;
-  PAYLOAD: number;
-  ROCKET_BODY: number;
+  ACCIDENTAL: number;
+  AERODYNAMICS: number;
+  ANOMALOUS: number;
+  COLLISION: number;
+  DELIBERATE: number;
+  ELECTRICAL: number;
+  EXPLOSION: number;
+  PROPULSION: number;
+  SMALL_IMPACTOR: number;
   UNKNOWN: number;
 };
 
 export async function getStatsMonthlyFragmentationEventsByObjectType(query?: TypeGetStatsMonthlyOrganizationsParams): Promise<StatsMonthlyFragmentationEventsByObjectType[]> {
-  const { data } = await Api.getStatsMonthlyFragmentationEventsByObjectType(query);
+  const { data } = await Api.getStatsMonthlyFragmentationEventsByFragmentationType(query);
 
   const groupedByMonth = groupBy(data, 'month');
 
   const groupedData = Object.entries(groupedByMonth).map(([month, events]) => {
-    const DEBRIS = events.find(event => event.objectType === 'Debris')?.count || 0;
-    const PAYLOAD = events.find(event => event.objectType === 'Payload')?.count || 0;
-    const ROCKET_BODY = events.find(event => event.objectType === 'Rocket Body')?.count || 0;
-    const UNKNOWN = events.find(event => event.objectType === 'Unknown')?.count || 0;
+    const ACCIDENTAL = events.find(event => event.fragmentationType === 'Accidental')?.count || 0;
+    const AERODYNAMICS = events.find(event => event.fragmentationType === 'Aerodynamics')?.count || 0;
+    const ANOMALOUS = events.find(event => event.fragmentationType === 'Anomalous')?.count || 0;
+    const COLLISION = events.find(event => event.fragmentationType === 'Collision')?.count || 0;
+    const DELIBERATE = events.find(event => event.fragmentationType === 'Deliberate')?.count || 0;
+    const ELECTRICAL = events.find(event => event.fragmentationType === 'Electrical')?.count || 0;
+    const EXPLOSION = events.find(event => event.fragmentationType === 'Explosion')?.count || 0;
+    const PROPULSION = events.find(event => event.fragmentationType === 'Propulsion')?.count || 0;
+    const SMALL_IMPACTOR = events.find(event => event.fragmentationType === 'Small Impactor')?.count || 0;
+    const UNKNOWN = events.find(event => event.fragmentationType === 'Unknown')?.count || 0;
+
     return {
       month: dayjs(month).format('MM/YYYY'),
-      DEBRIS,
-      PAYLOAD,
-      ROCKET_BODY,
+      ACCIDENTAL,
+      AERODYNAMICS,
+      ANOMALOUS,
+      COLLISION,
+      DELIBERATE,
+      ELECTRICAL,
+      EXPLOSION,
+      PROPULSION,
+      SMALL_IMPACTOR,
       UNKNOWN,
     };
   });
--- src/components/performance-monitoring/conjunction-events-monitoring/MonitoringFragmentationEventsByTypeMonthly.tsx
+++ src/components/performance-monitoring/conjunction-events-monitoring/MonitoringFragmentationEventsByTypeMonthly.tsx
@@ -6,37 +6,74 @@ import { MonthlyBarChart } from '@/components/charts/monthly-bar/MonthlyBar';
 import { DataTable } from '@/components/DataTable';
 import { Scrollable } from '@/components/Scrollable';
 
-import { reentryEventsByTypeMonthlyColumns } from './data-table/MonitoringReentryEventsByTypeMonthlyDataTableColumns';
+import { fragmentationEventsByTypeMonthlyColumns } from './data-table/MonitoringFragmentationEventsByTypeMonthlyDataTableColumns';
 
 const MonitoringFragmentationEventsByTypeMonthly = async () => {
-  const t = await getTranslations('Charts.Monthly_conjunction_events');
+  const t = await getTranslations('Charts.Monthly_fragmentation_events');
   const data = await getStatsMonthlyFragmentationEventsByObjectType();
+
   const datasets = {
     labels: data.map(({ month }) => month),
     datasets: [
       {
-        label: 'Payload',
-        data: data.map(({ PAYLOAD }) => PAYLOAD),
+        label: t('accidental'),
+        data: data.map(({ ACCIDENTAL }) => ACCIDENTAL),
         borderColor: chartPalette.nspocBlue,
         backgroundColor: chartPalette.nspocBlue,
       },
       {
-        label: 'Rocket Body',
-        data: data.map(({ ROCKET_BODY }) => ROCKET_BODY),
+        label: t('aerodynamics'),
+        data: data.map(({ AERODYNAMICS }) => AERODYNAMICS),
         borderColor: chartPalette.nspocRed,
         backgroundColor: chartPalette.nspocRed,
       },
       {
-        label: 'Debris',
-        data: data.map(({ DEBRIS }) => DEBRIS),
+        label: t('anomalous'),
+        data: data.map(({ ANOMALOUS }) => ANOMALOUS),
         borderColor: chartPalette.nspocYellow,
         backgroundColor: chartPalette.nspocYellow,
       },
       {
-        label: 'Unknown',
-        data: data.map(({ UNKNOWN }) => UNKNOWN),
+        label: t('collision'),
+        data: data.map(({ COLLISION }) => COLLISION),
         borderColor: chartPalette.nspocGreen,
         backgroundColor: chartPalette.nspocGreen,
+      },
+      {
+        label: t('deliberate'),
+        data: data.map(({ DELIBERATE }) => DELIBERATE),
+        borderColor: chartPalette.lightPurple,
+        backgroundColor: chartPalette.lightPurple,
+      },
+      {
+        label: t('electrical'),
+        data: data.map(({ ELECTRICAL }) => ELECTRICAL),
+        borderColor: chartPalette.turquoise,
+        backgroundColor: chartPalette.turquoise,
+      },
+      {
+        label: t('explosion'),
+        data: data.map(({ EXPLOSION }) => EXPLOSION),
+        borderColor: chartPalette.lightBlue,
+        backgroundColor: chartPalette.lightBlue,
+      },
+      {
+        label: t('propulsion'),
+        data: data.map(({ PROPULSION }) => PROPULSION),
+        borderColor: chartPalette.midBlue,
+        backgroundColor: chartPalette.midBlue,
+      },
+      {
+        label: t('small_impactor'),
+        data: data.map(({ SMALL_IMPACTOR }) => SMALL_IMPACTOR),
+        borderColor: chartPalette.brightBlue,
+        backgroundColor: chartPalette.brightBlue,
+      },
+      {
+        label: t('unknown'),
+        data: data.map(({ UNKNOWN }) => UNKNOWN),
+        borderColor: chartPalette.orange,
+        backgroundColor: chartPalette.orange,
       },
     ],
   };
@@ -49,12 +86,12 @@ const MonitoringFragmentationEventsByTypeMonthly = async () => {
         ariaLabel="Fragmentation events by type monthly"
         yAxisTitle={t('y_axis_title')}
         xAxisTitle={t('x_axis_title')}
-        legend={t('object_type')}
+        legend={t('fragmentation_type')}
         name="fragmentation-events-by-type-monthly"
       />
       <Scrollable>
         <DataTable
-          columns={reentryEventsByTypeMonthlyColumns}
+          columns={fragmentationEventsByTypeMonthlyColumns}
           data={data.reverse()}
           ariaLabel="Information on Fragmentation Events by type Monthly"
         />
