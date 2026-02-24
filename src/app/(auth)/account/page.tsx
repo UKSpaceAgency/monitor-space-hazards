54427bf48ee275e448a1687c8c383d2a6121b2d1
Philip <nowakf@gmail.com>
2026-02-24 16:58:01 +0100

    feat: hidden alert settings

--- src/app/(auth)/account/page.tsx
+++ src/app/(auth)/account/page.tsx
@@ -5,7 +5,7 @@ import { getTranslations } from 'next-intl/server';
 import { getSession } from '@/actions/getSession';
 import { getUsersMe } from '@/actions/getUsersMe';
 import TaskList from '@/ui/task-list/task-list';
-import { isAgencyApprover, isAgencyUser, isAnalysist, isOrgAdmin, isSuperAdmin } from '@/utils/Roles';
+import { isAgencyApprover, isAnalysist, isOrgAdmin, isSuperAdmin } from '@/utils/Roles';
 
 export const metadata: Metadata = {
   title: 'Your account information',
@@ -60,14 +60,14 @@ export default async function AccountPage() {
               title: t('change_your_notification_settings.conjunction_event_notification_settings'),
               href: '/account/notification-settings',
             },
-            ...(isAgencyUser(role)
-              ? [
-                  {
-                    title: t('change_your_notification_settings.alert_settings'),
-                    href: '/account/alert-settings',
-                  },
-                ]
-              : []),
+            // ...(isAgencyUser(role)
+            //   ? [
+            //       {
+            //         title: t('change_your_notification_settings.alert_settings'),
+            //         href: '/account/alert-settings',
+            //       },
+            //     ]
+            //   : []),
           ]}
         />
         {isOrgAdmin(role) && (
